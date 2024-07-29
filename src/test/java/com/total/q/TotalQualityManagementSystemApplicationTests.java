533874a91aa8afb006204fe1c4171b847aaf5aac
Nikhil-ai-dev <nikhil.nn245@gmail.com>
2024-07-29 22:05:48 +0530

    #Comment Initial Springboot set up

---
+++ src/test/java/com/total/q/TotalQualityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.total.q;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalQualityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
